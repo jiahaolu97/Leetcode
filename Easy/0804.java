31d30fcc25ed7d6a2db42c21394225a6c95c98c6
JiaHao Lu <38347064+RoyLJH@users.noreply.github.com>
2018-05-08 15:03:14 +0800

    Unique Morse Code Words

---
+++ Easy/0804.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        HashSet<String> set = new HashSet<String>();
+        for(int i=0;i<words.length;i++){
+            String w = words[i];
+            StringBuffer sb = new StringBuffer();
+            for(int j=0;j<w.length();j++){
+                int idx = (int)(words[i].charAt(j)-'a');
+                sb.append(morse[idx]);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
